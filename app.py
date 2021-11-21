46abc21ececf6dad9ce6bdf43a8dd531cc2effff
Chris <chris@Chriss-MacBook-Pro.local>
2021-11-21 02:19:41 +0200

    Add dropdown and vaccination percentage

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import math
+
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 
@@ -6,8 +8,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    r = requests.get(f"https://covid-api.mmediagroup.fr/v1/vaccines")
+    data = r.json()
+    countries = list(data.keys())
     if request.method == 'GET':
-        return render_template('index.html', people_vaccinated=0)
+        return render_template('index.html', people_vaccinated=0, countries=countries)
 
     if request.method == 'POST':
         country = request.form['country']
@@ -19,9 +24,11 @@ def home():
         data = r.json()
         try:
             people_vaccinated = data["All"]["people_vaccinated"]
+            population = data["All"]["population"]
+            percentage_vaccinated = math.floor(people_vaccinated / population * 100)
         except KeyError:
-            return render_template('index.html', people_vaccinated=-1)
-        return render_template('index.html', country_selected=country, people_vaccinated=people_vaccinated)
+            return render_template('index.html', people_vaccinated=-1, countries=countries)
+        return render_template('index.html', country_selected=country, people_vaccinated=people_vaccinated, percent=percentage_vaccinated, countries=countries)
 
 
 # app.run(debug=True, port=8080)
