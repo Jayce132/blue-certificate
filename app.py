5bef4a51eba20eff8254d0f45bd9859cea4a4a84
Chris <chris@Chriss-MacBook-Pro.local>
2021-11-21 10:29:43 +0200

    Remove countries with bad api data, change style

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import math
-import urllib
 
 from flask import Flask, render_template, request, redirect, url_for
 import requests
@@ -11,15 +10,20 @@ app = Flask(__name__)
 def home():
     r = requests.get(f"https://covid-api.mmediagroup.fr/v1/vaccines")
     data = r.json()
+
     # wrong api data for these
-    countries = [country for country in list(data.keys()) if
-                 country != "US (Aggregate)"
-                 and country != "Korea, South"
-                 and country != "Timor-Leste"
-                 and country[0:4] != "Cote"
-                 and country != "Congo (Brazzaville)"
-                 and country != "Congo (Kinshasa)"
-                 and country != ""]
+    # countries = [country for country in list(data.keys()) if
+    #              country != "US (Aggregate)"
+    #              and country != "Korea, South"
+    #              and country != "Timor-Leste"
+    #              and country[0:4] != "Cote"
+    #              and country != "Congo (Brazzaville)"
+    #              and country != "Congo (Kinshasa)"
+    #              and country != ""]
+
+    countries = [country for country in list(data.keys()) if data[country]["All"].get('population') if
+                 data[country]["All"].get('people_vaccinated')]
+
     if request.method == 'GET':
         return render_template('index.html', people_vaccinated=0, countries=countries)
 
@@ -27,17 +31,19 @@ def home():
         country = request.form['country']
         if country[0:2] != "US":
             country = country.lower().title().replace("And", "and").replace("The", "the")
-        print(country)
         r = requests.get(f"https://covid-api.mmediagroup.fr/v1/vaccines?country={country}")
         data = r.json()
+
         try:
             people_vaccinated = data["All"]["people_vaccinated"]
+            pvformat = f"{people_vaccinated:,}"
             population = data["All"]["population"]
             percentage_vaccinated = math.floor(people_vaccinated / population * 100)
         except KeyError:
             return render_template('index.html', people_vaccinated=-1, countries=countries)
+
         return render_template('index.html', country_selected=country, people_vaccinated=people_vaccinated,
-                               percent=percentage_vaccinated, countries=countries)
+                               percent=percentage_vaccinated, countries=countries, format_vaccinated=pvformat)
 
 
-# app.run(debug=True, port=8080)
+app.run(debug=True, port=8080)
